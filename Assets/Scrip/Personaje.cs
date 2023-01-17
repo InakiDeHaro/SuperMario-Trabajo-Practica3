ffafb2e44e10bb8633901216e4b6832ea305b17e
InakiDeHaro <121123337+InakiDeHaro@users.noreply.github.com>
2023-01-17 19:07:53 +0100

    19:07/17/01
    ANDA,(completo)SALTO(incompleto)

--- Assets/Scrip/Personaje.cs
+++ Assets/Scrip/Personaje.cs
@@ -10,11 +10,13 @@ public class Personaje : MonoBehaviour
     private Rigidbody2D rg;
     private BoxCollider2D hitbox;
     SpriteRenderer spriter;
-
+    Animator anima;
     // Start is called before the first frame update
     void Start()
     {
         rg = GetComponent<Rigidbody2D>();
+        anima = GetComponent<Animator>();
+        spriter = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -25,19 +27,28 @@ public class Personaje : MonoBehaviour
         if (direction > 0f)
         {
             rg.velocity = new Vector2(direction * speed, rg.velocity.y);
+            spriter.flipX = false;
         }
         else if (direction < 0f)
         {
             rg.velocity = new Vector2(direction * speed, rg.velocity.y);
+            spriter.flipX = true;
         }
         else
         {
             rg.velocity = new Vector2(0, rg.velocity.y);
         }
+        //direction
+
         //jump
         if (Input.GetButtonDown("Jump"))
         {
             rg.velocity = new Vector2(rg.velocity.x, jumpspeed);
         }
+        //animation "run"
+        anima.SetFloat("spd", Mathf.Abs(direction));
+        //animation "jump"
+
+        //animation "fall"
     }
 }
