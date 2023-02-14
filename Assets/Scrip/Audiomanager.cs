b92cbb11d4ef4a59c11dd210ba4ca3c342847078
InakiDeHaro <121123337+InakiDeHaro@users.noreply.github.com>
2023-02-14 13:12:08 +0100

    13:11/14/02/2023
    musica?
    botones: no
    temporizador: no
    Menu: "no"

---
+++ Assets/Scrip/Audiomanager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audiomanager : MonoBehaviour
+{
+    public Sound[] musicSounds, sfxSounds;
+    public AudioSource musicSource, SfxSource;
+}
---
+++ Assets/Sound/Sound.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound : MonoBehaviour
+{
+    public string Name;
+    public AudioClip clip;
+}
+ 
+
